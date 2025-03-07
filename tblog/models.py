97a6ad02239cdfcd83616d700413519e305dba9c
betsekpoe <193097046+betsekpoe@users.noreply.github.com>
2025-03-07 20:37:27 +0000

    built the homepage and loaded it into django; Got the blogs_list working to pull blogs from the database. Made a dummy container for PostCards.

--- tblog/models.py
+++ tblog/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import datetime
+from os import truncate
 
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import truncatewords
+from django.utils import timezone
+from django.utils.text import Truncator
+from django.utils.timezone import now
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(default=now)
+    image = models.ImageField(upload_to="tblog/images/")
+    content = models.TextField()
+    tags = models.CharField(max_length=255, help_text="Comma-separated tags")
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def is_recent_post(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=5)
+
+    @property
+    def is_today(self):
+        return self.date == timezone.now().date()
+
+    @property
+    def excerpt(self):
+        return Truncator(self.content).words(30)
--- tsekpoe/settings.py
+++ tsekpoe/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+# DEFAULT IMPORTS
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tblog.apps.TblogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'tsekpoe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['tblog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CUSTOM MADE BY ME
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
